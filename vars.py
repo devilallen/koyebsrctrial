e8670441b2d7c35ee1905bc8ec3ac39aeb9813d5
devilallen <85722023+devilallen@users.noreply.github.com>
2025-01-12 21:39:53 +0530

    Create vars.py

---
+++ vars.py
@@ -0,0 +1,5 @@
+from os import environ
+
+API_ID = int(environ.get("API_ID", "29565691"))
+API_HASH = environ.get("API_HASH", "e093998dbea2e7930d93a5ea14ba8cda")
+BOT_TOKEN = environ.get("BOT_TOKEN", "6786282659:AAFM0ytecseVrerlTTh7VODFMz-phbW7bfs")
