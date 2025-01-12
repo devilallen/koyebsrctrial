c7c57a6b63c8f9ee7fa04489b1ca876135d2befb
devilallen <85722023+devilallen@users.noreply.github.com>
2025-01-12 21:59:09 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,28 @@
+from pyrogram import Client
+from config import API_ID, API_HASH, BOT_TOKEN
+
+class Bot(Client):
+
+    def __init__(self):
+        super().__init__(
+            "techvj login",
+            api_id=API_ID,
+            api_hash=API_HASH,
+            bot_token=BOT_TOKEN,
+            plugins=dict(root="TechVJ"),
+            workers=50,
+            sleep_threshold=10
+        )
+
+      
+    async def start(self):
+            
+        await super().start()
+        print('Bot Started Powered By @officialharsh_g')
+
+    async def stop(self, *args):
+
+        await super().stop()
+        print('Bot Stopped Bye')
+
+Bot().run()
